8f742f3fe1b902b6f34e74b439a6dd57eaccc220
Don Ward <10is0420@gmail.com>
2020-04-05 16:18:39 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,9 +2,9 @@ class Person
   attr_accessor :attribute, :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
   
   def initialize(attribute)
-    @attribute = attribute
+    @attribute = attributes.each {|key, value| self.send(("#{key}="), value)}
   end
   
-  attributes.each {|key, value| self.send(("#{key}="), value)}
+  
   
 end
